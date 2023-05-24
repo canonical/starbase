f4ad7279dc7da0e18e6a74fb87ef528400b6b96a
Tiago Nobrega <tiago.nobrega@canonical.com>
2023-05-24 15:07:18 -0300

    tweaks for the linters

--- starcraft/__init__.py
+++ starcraft/__init__.py
@@ -18,12 +18,22 @@ from typing import List, Optional, Any
 
 try:
     from ._version import __version__
-except ImportError:
+except ImportError:  # pragma: no cover
+    from importlib.metadata import version, PackageNotFoundError
+
     try:
-        from importlib.metadata import version, PackageNotFoundError
         __version__ = version("starcraft")
     except PackageNotFoundError:
         __version__ = "dev"
+
+
+def hello(people: Optional[List[Any]] = None) -> None:
+    """Says hello."""
+    print("Hello *craft team!")
+    if people:
+        for person in people:
+            print(f"Hello {person}!")
+
 
 __all__ = [
     "__version__",
