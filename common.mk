0313865cd4384417ddb012c256623762c20db5cb
jahn-junior <jj.coldiron@canonical.com>
2026-04-14 11:28:01 -0700

    docs: fix sphinx-autobuild loop

--- common.mk
+++ common.mk
@@ -8,6 +8,7 @@ SOURCES=$(wildcard *.py) $(PROJECT) tests
 export DOCS_BUILDDIR ?= _build
 export DOCS_VENVDIR ?= ../.venv
 export VALEDIR ?= $(VENVDIR)/lib/python*/site-packages/vale
+export SPHINXOPTS ?= -c . -d .sphinx/.doctrees -j auto --ignore "../.venv/*" --ignore "reference/commands/*"
 
 ifneq ($(OS),Windows_NT)
 	OS := $(shell uname)
