cb1601465c56908c4795f5a21ca8ac9c9274273f
Michael DuBelko <michael.dubelko@canonical.com>
2026-06-15 15:03:43 -0700

    build(docs): override docs-clean target
    The `docs-clean` target in the docs Makefile has an ambiguous `test`
    command that fails if we export a temp directory to it. To work around
    that, override the target completely, so we don't require a temp dir to
    delete in the first place.

--- common.mk
+++ common.mk
@@ -8,6 +8,8 @@ SOURCES=$(wildcard *.py) $(PROJECT) tests
 export DOCS_BUILDDIR ?= _build
 export DOCS_VENVDIR ?= ../.venv
 export VALE_DIR ?= $(DOCS_VENVDIR)/lib/python*/site-packages/vale
+export DEV_DIR ?= docs/_dev
+export VALE_CONFIG ?= $(DEV_DIR)/vale.ini
 export SPHINX_AUTOBUILD_OPTS ?= --ignore "$(DOCS_VENVDIR)/*" --ignore "reference/commands/*" -D=llms_txt_enabled=0
 
 ifneq ($(OS),Windows_NT)
@@ -296,8 +298,10 @@ docs-setup: setup-docs
 # we pass a null dir instead.
 .PHONY: docs-clean
 docs-clean:  ##- Clean the temporary files used in documentation
-	VENVDIR=$(mktemp)
-	$(MAKE) -C docs clean --no-print-directory
+	$(MAKE) -C docs clean-doc --no-print-directory
+	rm -rf $(DEV_DIR)/node_modules/
+	rm -rf $(DEV_DIR)/styles
+	rm -rf $(VALE_CONFIG)
 
 # Override for `help` target in docs project
 .PHONY: docs-help
